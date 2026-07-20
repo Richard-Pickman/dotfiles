fb81e1a2e83b789ef7079dbf08fafa9cc7370120
jason <fake@email.com>
2026-07-20 13:26:22 -0400

    remove deleted scripts

--- arch_full_collect.bash
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-pacman -Qqen
-pacman -Qqen >~/dotfiles/arch/arch_full_package_list.txt
--- arch_minimal_install.bash
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-sudo pacman -S --needed - <~/dotfiles/arch/arch_minimal_package_list.txt
