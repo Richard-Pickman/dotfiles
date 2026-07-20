623ba3cee6d94cd61e8efb05a68dc926bda3d734
jason <fake@email.com>
2026-07-20 13:18:00 -0400

    remove arch setup from this project making this a bash terminal configurator and assuming nvim is installed

--- create_symlinks.bash
+++ create_symlinks.bash
@@ -2,25 +2,24 @@
 
 source "constants"
 
-if [[ -f "$base/$master_file" ]]; then
-	echo "This appears to be the master system due to the presence of $base/$master_file."
-	echo "Aborting symlink creation."
-	exit 1
-else
-	echo "This does not appear to be the master system. Installing configuration files."
-fi
+#if [[ -f "$base/$master_file" ]]; then
+#	echo "This appears to be the master system due to the presence of $base/$master_file."
+#	echo "Aborting symlink creation."
+#	exit 1
+#else
+#	echo "This does not appear to be the master system. Installing configuration files."
+#fi
 
-if ! ln -s "$base/bash/.bashrc" "$HOME/.bashrc"; then
-  echo "remove or rename .bashrc"
+if ! ln -s "$base/bashrc" "$HOME/.bashrc"; then
+  echo "$HOME/.bashrc already exists. Either remove or rename it."
   exit 1
+else
+  echo "$HOME/.bashrc linked to $base/bashrc"
 fi
 
 if ! ln -s "$base/nvim" "$HOME/.config/nvim"; then
   echo "remove or rename nvim symlink"
   exit 1
-fi
-
-if ! ln -s "$base/terminator/" "$HOME/.config/terminator"; then
-  echo "remove of rename terminator symlink"
-  exit 1
+else
+  echo "$HOME/.config/nvim linked to $base/nvim"
 fi
--- git_push.bash
+++ git_push.bash
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+source constants
+
+if [ ! -f "$master_file" ]; then
+	echo "This does not appear to be the master system!"
+	exit 1
+fi
+
 git push -u origin "$(git branch --show-current)"
